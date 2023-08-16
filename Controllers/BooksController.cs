4f8452b2c1f8d0ecc84ed5f0bc720d9d758aaf94
FelixW <58850440+FelixJustice@users.noreply.github.com>
2023-08-16 19:13:24 +0300

    Removed the /api/ from url

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Books.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class BooksController : ControllerBase
     {
